33ef33dc7c22bf4a6c55a4bad1b94f2f21af32f1
Dylan DesRosier <dylan@pooltogether.com>
2023-07-05 20:32:08 +0100

    Migrate vault

--- src/Claimer.sol
+++ src/Claimer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
 import { SD59x18 } from "prb-math/SD59x18.sol";
 import { UD2x18 } from "prb-math/UD2x18.sol";
--- test/libraries/LinearVRGDALib.t.sol
+++ test/libraries/LinearVRGDALib.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
 import { UD2x18, ud2x18 } from "prb-math/UD2x18.sol";
